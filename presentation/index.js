6237b2036a300bdf31251f0b559622665da2ca5e
Aravind Balla <bsaaravind@gmail.com>
2019-10-10 19:32:18 +0530

    basic content

--- presentation/index.js
+++ presentation/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 
 // Import Spectacle Core tags
 import {
+  Appear,
   BlockQuote,
   Cite,
   Deck,
@@ -13,15 +14,16 @@ import {
   Notes,
   Quote,
   Slide,
-  Text
+  Text,
+  MarkdownSlides,
 } from 'spectacle';
 
 // Import theme
 import createTheme from 'spectacle/lib/themes/default';
 
 const images = {
-  formidagon: require('../assets/formidable-logo.svg'),
-  goodWork: require('../assets/good-work.gif')
+  gitBlame: require('../assets/git-blame.png'),
+  goodWork: require('../assets/good-work.gif'),
 };
 
 // Require CSS
@@ -31,77 +33,82 @@ const theme = createTheme(
   {
     primary: 'white',
     secondary: '#1F2022',
-    tertiary: '#03A9FC',
-    quaternary: '#CECECE'
+    tertiary: '#639',
+    quaternary: '#CECECE',
   },
   {
-    primary: 'Montserrat',
-    secondary: 'Helvetica'
+    primary: 'Avenir Next',
+    secondary: 'Helvetica',
   }
 );
 
 export default class Presentation extends React.Component {
   render() {
     return (
-      <Deck
-        transition={['zoom', 'slide']}
-        transitionDuration={500}
-        theme={theme}
-      >
+      <Deck transition={['zoom', 'slide']} transitionDuration={500} theme={theme}>
         <Slide transition={['zoom']} bgColor="primary">
-          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            Spectacle Boilerplate
+          <Text>Hi, I'm</Text>
+          <Heading size={1} fit caps lineHeight={1} textColor="tertiary">
+            Aravind Balla
           </Heading>
-          <Text margin="10px 0 0" textColor="tertiary" fit bold>
-            open the presentation/index.js file to get started
-          </Text>
-        </Slide>
-        <Slide bgColor="secondary">
-          <Image src={images.formidagon} width={800} />
-        </Slide>
-        <Slide transition={['fade']} bgColor="tertiary">
-          <Heading size={6} textColor="primary" caps>
-            Typography
-          </Heading>
-          <Heading size={1} textColor="secondary">
-            Heading 1
-          </Heading>
-          <Heading size={2} textColor="secondary">
-            Heading 2
-          </Heading>
-          <Heading size={3} textColor="secondary">
-            Heading 3
-          </Heading>
-          <Heading size={4} textColor="secondary">
-            Heading 4
-          </Heading>
-          <Heading size={5} textColor="secondary">
-            Heading 5
-          </Heading>
-          <Text size={6} textColor="secondary">
-            Standard text
-          </Text>
-        </Slide>
-        <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>
-            Standard List
-          </Heading>
-          <List>
-            <ListItem bulletStyle="star">Item 1</ListItem>
-            <ListItem bulletStyle="cross">Item 2</ListItem>
-            <ListItem>Item 3</ListItem>
-            <ListItem>Item 4</ListItem>
-          </List>
-        </Slide>
-        <Slide transition={['fade']} bgColor="secondary" textColor="primary">
-          <BlockQuote>
-            <Quote>Example Quote</Quote>
-            <Cite margin="10px 0 0 30px">Author</Cite>
-          </BlockQuote>
+          <Text>👨🏻‍💻JS Engineer at Paperpile</Text>
+          <Text
+            textFont="monospace"
+            margin="30px 0 0 0"
+            textColor="#444"
+            textSize="1.5rem"
+          >{`{twitter|github}.com/aravindballa`}</Text>
         </Slide>
         <Slide>
-          <Image src={images.goodWork} width={500} />
-          <Notes>gifs work too</Notes>
+          <Image src={images.gitBlame} width={500} />
+        </Slide>
+        <Slide bgColor="tertiary">
+          <Heading size={1} fit caps lineHeight={1} textColor="primary">
+            Build blazing fast portfolios
+          </Heading>
+        </Slide>
+        <Slide transition={['fade']}>
+          <Text>Every developer* should have one</Text>
+          <Text margin="30px 0 0 0" textColor="#444" textSize="1.5rem">
+            *You too! 🤷🏻‍♂️
+          </Text>
+        </Slide>
+        <Slide transition={['fade']}>
+          <Heading size={6} textColor="secondary" caps>
+            Why don't we do it?
+          </Heading>
+          <List>
+            <Appear fid="1">
+              <ListItem>I'm not an expert</ListItem>
+            </Appear>
+            <Appear fid="2">
+              <ListItem>Someone already wrote about it</ListItem>
+            </Appear>
+            <Appear fid="3">
+              <ListItem>Why write the same thing?</ListItem>
+            </Appear>
+          </List>
+        </Slide>
+        <Slide transition={['fade']}>
+          <Heading size={6} textColor="secondary" caps>
+            Why should you <span style={{ fontStyle: 'italic' }}>actually</span> do it?
+          </Heading>
+          <List>
+            <Appear fid="1">
+              <ListItem>When you try explaining concepts, you learn it better.</ListItem>
+            </Appear>
+            <Appear fid="2">
+              <ListItem>Document your learnings</ListItem>
+            </Appear>
+            <Appear fid="3">
+              <ListItem>
+                If someone looks at your blog/portfolio, they know you are familiar with things.
+              </ListItem>
+            </Appear>
+          </List>
+        </Slide>
+        <Slide>
+          <Text>Not just tech, write everything 💁🏻‍♂️</Text>
         </Slide>
       </Deck>
     );
